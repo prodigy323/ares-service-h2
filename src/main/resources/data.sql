74a3ea5ce4b25629a328ee046ce8374c9d062972
Joe Hong <jhng323@gmail.com>
2018-11-11 10:48:20 -0500

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into hero (first_name, last_name, code_name, email, team) values
+('Tony', 'Stark', 'Ironman', 'ironman@avengers.com', 'Avengers'),
+('Bruce', 'Banner', 'Hulk', 'hulk@avengers.com', 'Avengers'),
+('Bruce', 'Wayne', 'Batman', 'batman@avengers.com', 'JLA'),
+('Clark', 'Kent', 'Superman', 'superman@avengers.com', 'JLA'),
+('Scott', 'Summers', 'Cyclops', 'cyclops@avengers.com', 'XMen'),
+('James', 'Logan', 'Wolverine', 'wolverine@avengers.com', 'XMen');
